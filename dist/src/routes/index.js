2590909c8645f429f4a1ab7ac6a21541f950937f
JonathanUzielMedina22 <uziel.jonathan22@gmail.com>
2025-04-01 12:00:51 -0700

    Agregar conexión a BD

--- dist/src/routes/index.js
+++ dist/src/routes/index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const productRoutes_1 = __importDefault(require("./productRoutes.js"));
+const productRoutes_1 = __importDefault(require("./productRoutes"));
 const apiRouter = (0, express_1.Router)();
 apiRouter.use('/product', productRoutes_1.default);
 apiRouter.get('/', (req, res) => {
